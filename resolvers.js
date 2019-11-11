858b14480f57575f7a709dbca0c6b10903f182d0
quyencao <boy20195@gmail.com>
2019-11-11 11:32:32 +0700

    new commit

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,7 @@
+ 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { AuthenticationError, UserInputError } = require("apollo-server-lambda");
 const models = require("./models");
 
 const resolver = {
@@ -12,12 +16,51 @@ const resolver = {
     },
     Mutation: {
         register: (_, args) => {
-            return models.User.create({
-                email: args.email,
-                password: args.password
-            }).then(user => {
-                return user.dataValues;
-            }).catch(err => {
+            return models.User.findOne({ where: { email: args.email } })
+            .then(user => {
+                if (user) {
+                    throw new UserInputError("Email is already taken");
+                }
+
+                return bcrypt.genSalt(10);
+            })
+            .then(salt => {
+                return bcrypt.hash(args.password, salt);
+            })
+            .then(hash => {
+                return models.User.create({
+                    email: args.email,
+                    password: hash
+                });
+            })
+            .then(user => user.dataValues)
+            .catch(err => {
+                throw err;
+            })
+        },
+        login: (_, args) => {
+            return models.User.findOne({ where: { email: args.email } })
+            .then(user => {
+                if (!user) {
+                    throw new AuthenticationError(
+                        'No user found with this login credentials.',
+                    );
+                }
+
+                return bcrypt.compare(args.password, user.dataValues.password);
+            })
+            .then(result => {
+                if (!result) {
+                    throw new AuthenticationError(
+                        'No user found with this login credentials.',
+                    );
+                }
+
+                const token = jwt.sign({ email: args.email }, "secretkey", { algorithm: "HS256", expiresIn: "1h" });
+
+                return { token };
+            })
+            .catch(err => {
                 throw err;
             })
         }
--- schema.js
+++ schema.js
@@ -3,10 +3,15 @@ const { gql } = require("apollo-server-lambda");
 const typeDefs = gql`
 type Mutation {
   register(email: String!, password: String!): User!
+  login(email: String!, password: String!): Token!
 }
 
 type Query {
   getUsers: [User!]
+}
+
+type Token {
+  token: String!
 }
 
 type User {
